50e18e2f5e1a26199ddd8d73602c569047392eaf
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2025-02-13 18:14:31 +0530

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i =0; i<nums.size();i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        if(target>nums[nums.size()-1]){
+            return nums.size();
+        }
+        return 0;
+        
+    }
+};
