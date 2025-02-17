754c40eba920c7fa470d5a4182aaedeac3da8b3a
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2025-02-17 10:43:46 +0530

    Create  Code Testcase Testcase Test Result 2215. Find the Difference of Two Arrays.cpp

---
+++ Code Testcase Testcase Test Result 2215. Find the Difference of Two Arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+         unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+
+        vector<int> unique1, unique2;
+
+        for (int num : set1) {
+            if (set2.find(num) == set2.end()) {
+                unique1.push_back(num);
+            }
+        }
+
+        for (int num : set2) {
+            if (set1.find(num) == set1.end()) {
+                unique2.push_back(num);
+            }
+        }
+
+        return {unique1, unique2};
+    }
+};
